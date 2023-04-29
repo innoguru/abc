351335bb25c2deb5c14daa0e9670f96a39fad2b5
innoguru <alar.kolk@gmail.com>
2023-04-29 22:45:00 +0300

    Create flan.py

---
+++ flan.py
@@ -0,0 +1,5 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-large")
+
+model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-large")
