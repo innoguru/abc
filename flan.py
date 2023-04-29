8fbcc39d89ae3169e947820c50a9ea44f441c8f9
innoguru <alar.kolk@gmail.com>
2023-04-29 23:09:37 +0300

    a

--- flan.py
+++ flan.py
@@ -1,5 +1,17 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-large")
-
 model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-large")
+
+def ask_question(question, context):
+    input_prompt = f"question: {question} context: {context}"
+    input_ids = tokenizer.encode(input_prompt, return_tensors="pt")
+    output_ids = model.generate(input_ids)
+    answer = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+    return answer
+
+context = "OpenAI is an artificial intelligence research lab consisting of the for-profit corporation OpenAI LP and its parent company, the non-profit OpenAI Inc. It was founded in December 2015 by Elon Musk, Sam Altman, and others."
+question = "Who founded OpenAI?"
+
+answer = ask_question(question, context)
+print(answer)
