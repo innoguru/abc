51d2ae3f25a79f6c23f1c07ae8f3443df0e1bc9a
innoguru <alar.kolk@gmail.com>
2023-04-30 12:51:48 +0300

    Create sticuna.py

---
+++ sticuna.py
@@ -0,0 +1,20 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("path/to/stable-vicuna-13b-applied")
+model = AutoModelForCausalLM.from_pretrained("path/to/stable-vicuna-13b-applied")
+model.half().cuda()
+
+prompt = """\
+### Human: Write a Python script for text classification using Transformers and PyTorch
+### Assistant:\
+"""
+
+inputs = tokenizer(prompt, return_tensors='pt').to('cuda')
+tokens = model.generate(
+ **inputs,
+ max_new_tokens=256,
+ do_sample=True,
+ temperature=1.0,
+ top_p=1.0,
+)
+print(tokenizer.decode(tokens[0], skip_special_tokens=True))
